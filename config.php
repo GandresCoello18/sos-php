3fc4bafafd8dd71d94495b2a733eedecee3b7026
GandresCoello18 <goyeselcoca@gmail.com>
2019-08-28 10:12:21 -0500

    cambiendo base de datos remoto

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 // https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'emergencia');
+define('DB_SERVER', 'remotemysql.com');
+define('DB_USERNAME', 'p7wbe4fKzY');
+define('DB_PASSWORD', '97UnoLaBA5');
+define('DB_NAME', 'p7wbe4fKzY');
  
 /* Attempt to connect to MySQL database */
 try{
